904f67d5a5df1409442eef8e260b7b014f918100
B. Maxwell Ruotsi <bmruotsi@gmail.com>
2016-09-12 15:32:24 -0400

    models and initial migration made

--- wellcom/wellcom_app/models.py
+++ wellcom/wellcom_app/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Well(models.Model):
+    name = models.CharField(max_length=200)
+    latitude = models.DecimalField(max_digits=11, decimal_places=7)
+    longitude = models.DecimalField(max_digits=11, decimal_places=7)
+    country = models.CharField(max_length=200)  # Maybe automatically populate from lat/lon
+    date_installed = models.DateField()
+    last_update = models.DateTimeField()
+    estimated_users = models.IntegerField()  # hidden
+    cost_usd = models.DecimalField(max_digits=8, decimal_places=2)  # hidden
+    contractor = models.CharField(max_length=200)  # hidden
+    flow_rate_lpm = models.DecimalField(max_digits=6, decimal_places=2)  # hidden
+
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        self.last_update = timezone.now()
+        return super(Well, self).save(*args, **kwargs)
+
+
+class Notes(models.Model):
+    well = models.ForeignKey(Well)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class DeviceData(models.Model):
+    well = models.ForeignKey(Well)
+    timestamp = models.DateTimeField()
+    temperature_c = models.DecimalField(max_digits=5, decimal_places=2)
+
+class WaterTest(models.Model):
+    """All parameters are recorded in mg/l unless otherwise stated"""
+
+    well = models.ForeignKey(Well)
+    date = models.DateField()
+    pH = models.DecimalField(max_digits=15, decimal_places=4)
+    turbidity_ntu = models.DecimalField(max_digits=15, decimal_places=4)
+    app_true_colour_hz = models.IntegerField()
+    conductivity_uscm = models.DecimalField(max_digits=15, decimal_places=4)
+    temperature_c = models.DecimalField(max_digits=5, decimal_places=2)
+    total_iron = models.DecimalField(max_digits=15, decimal_places=4)
+    calcium = models.DecimalField(max_digits=15, decimal_places=4)
+    magnesium = models.DecimalField(max_digits=15, decimal_places=4)
+    chloride = models.DecimalField(max_digits=15, decimal_places=4)
+    sulphate = models.DecimalField(max_digits=15, decimal_places=4)
+    suspended_solids = models.DecimalField(max_digits=15, decimal_places=4)
+    total_dissolved_solids = models.DecimalField(max_digits=15, decimal_places=4)
+    total_solids = models.DecimalField(max_digits=15, decimal_places=4)
+    total_alkalinity = models.DecimalField(max_digits=15, decimal_places=4)
+    total_hardness = models.DecimalField(max_digits=15, decimal_places=4)
+    calcium_hardness = models.DecimalField(max_digits=15, decimal_places=4)
+    magnesium_hardness = models.DecimalField(max_digits=15, decimal_places=4)
+    copper = models.DecimalField(max_digits=15, decimal_places=4)
+    nitrite_nitrogen = models.DecimalField(max_digits=15, decimal_places=4)
+    nitrate_nitrogen = models.DecimalField(max_digits=15, decimal_places=4)
+    fluoride = models.DecimalField(max_digits=15, decimal_places=4)
+    mpn_index_tc_per_deciliter = models.DecimalField(max_digits=15, decimal_places=4)
+    ammonia_nitrogen = models.DecimalField(max_digits=15, decimal_places=4)
+    manganese = models.DecimalField(max_digits=15, decimal_places=4)
+    aluminum = models.DecimalField(max_digits=15, decimal_places=4)

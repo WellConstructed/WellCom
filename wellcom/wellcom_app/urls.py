9536fb3dd68221b1d7e996acd1dd1dde1835a676
Maggie Correll <maggie.correll@gmail.com>
2016-09-13 10:57:52 -0400

    added urls and html pages for index, all wells, and  the individual well pages

--- wellcom/wellcom_app/urls.py
+++ wellcom/wellcom_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^allwells/$', views.all_wells, name='all_wells'),
+    url(r'^well/(?P<pk>[0-9]+)', views.well_detail, name='well_detail')
 ]
--- wellcom/wellcom_app/views.py
+++ wellcom/wellcom_app/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'index.html')
+
+
+def all_wells(request):
+    return render(request, 'all_wells.html')
+
+
+def well_detail(request):
+    return render(request, 'well_detail.html')

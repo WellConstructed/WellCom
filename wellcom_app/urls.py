933beeca068154e45efdef2bb2aaa3913c39dd76
Maggie Correll <maggie.correll@gmail.com>
2016-09-15 15:03:37 -0400

    changed landing page;

--- wellcom_app/urls.py
+++ wellcom_app/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^wells/$', views.wells, name='wells'),
+    url(r'^about/$', views.about_us, name='about_us'),
+    url(r'^$', views.wells, name='wells'),
     url(r'^well/(?P<well_id>[0-9]+)', views.well_detail, name='well_detail'),
 
     url(r'^graph_temp/', views.graph_temp, name='graph_temp'),

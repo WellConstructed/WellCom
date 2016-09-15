09883994e3d31d3745420bd0d547ac2ca096717e
Maggie Correll <maggie.correll@gmail.com>
2016-09-15 16:06:14 -0400

    added main map to home page with markers that indcate locations

--- wellcom_app/static/js/maps.js
+++ wellcom_app/static/js/maps.js
@@ -5,9 +5,10 @@ var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('all_well_map'), {
       center: {lat: 36.9930780, lng: -79.9046890},
-      zoom: 8
+      zoom: 8,
+      scrollwheel: false
     });
-  }
+
 
 
 $.ajax({
@@ -19,56 +20,42 @@ $.ajax({
     $wellData = data.results;
     console.log($wellData);
 
-
+      var infowindow = new google.maps.InfoWindow();
 
       var marker, i;
+
       for (i = 0; i < $wellData.length; i++) {
-          console.log('lat');
-          console.log(Number($wellData[i].latitude));
-          console.log('long');
-          console.log(Number($wellData[i].longitude));
+
           marker = new google.maps.Marker({
               // position: new google.maps.LatLng($wellData[i].latitude, $wellData[i].longitude),
               position: new google.maps.LatLng(Number($wellData[i].latitude), Number($wellData[i].longitude)),
               map: map
           });
-
-          infowindow = new google.maps.InfoWindow({
-               content: '<div id="content">' + '<strong>' + $wellData.name + '</strong>'
-            });
-
+          google.maps.event.addListener(marker, 'click', (function(marker, i) {
+              return function() {
+              infowindow.setContent('<strong>' + $wellData[i].name + '</strong>'
+                  + '<p> latitude:  ' + $wellData[i].latitude + '</p>' +
+                    '<p> longitude:  ' + $wellData[i].longitude + '</p>');
+              infowindow.open(map, marker);
+    }
+  })(marker, i));
 
       }
-
-      google.maps.event.addListener(marker, 'click', function() {
-          content: infowindow;
-          infowindow.open(map, marker);
-        });
-      //
-      // marker.addListener('click', function() {
-      //   infowindow.open(map, marker);
-      // });
-
 
   },
   error: function(xhr, textStatus, errorThrown) {
     // Handle error
     console.log('Error getting the Well Data');
   }
+
 });
 
+// infowindow = new google.maps.InfoWindow({
+//      content: '<div id="content">' + '<strong>' + $wellData[i].name + '</strong>'
+//   });
+//   google.maps.event.addListener(marker, 'click', function() {
+//         content: infowindow;
+//         infowindow.open(map, marker);
+//   })
 
-// var infowindow = new google.maps.InfoWindow();
-
-
-// new google.maps.event.addDomListener(window, "load", initMap);
-console.log('length');
-console.log($wellData.length);
-// Add locations onto map
-
-//
-// marker = new google.maps.Marker({
-//     // position: new google.maps.LatLng($wellData[i].latitude, $wellData[i].longitude),
-//     position: new google.maps.LatLng(-34.397, 150.644),
-//     map: map
-// })
+}

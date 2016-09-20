d166570c78a86839f6e83bc1709726206facf1a2
Maggie Correll <maggie.correll@gmail.com>
2016-09-20 15:19:39 -0400

    made changes

--- wellcom_app/static/js/well_detail.js
+++ wellcom_app/static/js/well_detail.js
@@ -3,7 +3,14 @@ $(document).ready(function() {
     $(".drop_header").click(function() {
         var $next = $(this).next(".drop_content");
         $(".drop_content").not($next).slideUp();
-        $next.slideToggle(500);
-        $(this).toggleClass('exp');
+        $next.slideToggle(300);
+      div.addEventListener('click', function(){
+        if(open){
+          icon.className = 'fa fa-arrow-down';
+        } else{
+          icon.className = 'fa fa-arrow-down open';
+        }
+        open = !open;
     });
+});
 });
--- wellcom_app/static/js/wells.js
+++ wellcom_app/static/js/wells.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('#all_well_data').dataTable();
 });
 
-jQuery(document).ready(function($) {
+$(document).ready(function($) {
     $(".clickable-row").click(function() {
         window.document.location = $(this).data("href");
     });

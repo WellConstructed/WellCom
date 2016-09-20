9c0adf58698465a3d74b7e9ea2d438978d4b7de5
B. Maxwell Ruotsi <bmruotsi@gmail.com>
2016-09-20 11:31:04 -0400

    removed test_graph from urls

--- wellcom_app/urls.py
+++ wellcom_app/urls.py
@@ -5,7 +5,4 @@ urlpatterns = [
     url(r'^about/$', views.about_us, name='about_us'),
     url(r'^$', views.wells, name='wells'),
     url(r'^wells/(?P<well_id>[0-9]+)', views.well_detail, name='well_detail'),
-
-    url(r'^graph_temp/', views.graph_temp, name='graph_temp'),
-
 ]

e401507417c825d372f945edc4fe1a75ba20cd88
Maggie Correll <maggie.correll@gmail.com>
2016-09-21 11:26:58 -0400

    down arrow moving with the page

--- wellcom_app/static/js/wells.js
+++ wellcom_app/static/js/wells.js
@@ -7,3 +7,10 @@ $(document).ready(function($) {
         window.document.location = $(this).data("href");
     });
 });
+
+$(window).scroll(function() {
+   if($(window).scrollTop() + $(window).height() == $(document).height()) {
+       console.log("bottom!");
+       $(document).find("i").toggleClass("fa fa-arrow-circle-down fa-2x").toggleClass("fa fa-arrow-circle-up fa-2x");
+   }
+});
